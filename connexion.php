1b9e5f50ce5e05fff6c2f47189a8b65d4b07a02b
Orianne <orianne.essienth@gmail.com>
2024-09-19 18:14:56 +0200

    composer.json + connexion

--- connexion.php
+++ connexion.php
@@ -1,18 +1,38 @@
 <?php
+    require_once 'vendor/autoload.php';
 
+    $env = Dotenv\Dotenv::createImmutable(__DIR__);
+    $env->load();
+
+    $trackingNumber = $_ENV['TRACKING_NUMBER'];
+    $apiKey=$_ENV['API_KEY'];
     //Adresse de l'api suivi de colis
-    $url = "https://api.laposte.fr/suivi/v2/idships/6A58894858306";
+    $url = "https://api.laposte.fr/suivi/v2/idships/$trackingNumber?lang=fr_FR";
 
     //Initialisation 
     $ch = curl_init();
 
+    curl_setopt($ch, CURLOPT_CAINFO, 'C:\cert\cacert.pem');
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: application/json',
+        'X-Okapi-Key: ' . $apiKey
+    ]);
 
     $response = curl_exec($ch);
+
+    if ($response === false) {
+        // En cas d'erreur, afficher l'erreur cURL
+        echo 'Erreur cURL : ' . curl_error($ch);
+    } /*else {
+        // Si la requête a réussi, afficher la réponse
+        echo 'Réponse de l\'API : ' . $response;
+    }
+*/
     curl_close($ch);
 
     $data = json_decode($response,true);
-    var_dump($response);
+    //var_dump($response);
 
 ?>
--- index.php
+++ index.php
@@ -3,18 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Etat colis</title>
 </head>
 <body>
     <?php
     include 'connexion.php';
-
-
-    if($isset($response) && is_array($response)) {
-        if(isset($response['shipment'])){
-            echo "<p>".$response['shipment']['properties']['event']."</p>";
+    $shipment = json_encode($data["shipment"],JSON_PRETTY_PRINT);
+    echo '<pre>';
+    var_dump($data);
+    //var_dump ($data['shipment']['timeline']);
+    echo '</pre>';
+    foreach($data['shipment']['timeline'] as $key => $value){
+        "<p>Etat ".$key." : ".$value."</p>";
+    }
+    if(isset($response)) {
+        //echo 'test1 passé';
+        if(isset($data['shipment'])){
+            echo 'test2 passé';
+            //"<p>".$data['shipment']['status']['event']."</p>";
+            
         }
         
+        //echo 
     }
     ?>
 </body>
