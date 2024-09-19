b8f90e1d1c7b4ca80563e14297562779cb134ab3
Orianne <orianne.essienth@gmail.com>
2024-09-19 19:51:06 +0200

    start of mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,21 @@
 <?php
-    require 'connexion.php'
+    require 'connexion.php';
+    require 'index.php';
 
     ini_set('display_errors',1);
     error_reporting(E_ALL);
 
-    $from =$_ENV['FROM']
+    $from=$_ENV['FROM'];
+    $to=$_ENV['TO'];
+
+    $subject="Suivi de votre colis.";
+
+    if($code === "DI1"){
+        $message="Votre colis a été livré !";
+        $mail->addAttachment('/images/happy.jpg');
+    } else{
+        $message="Le colis n°".$_ENV['TRACKING_NUMBER']." est toujours en cours de livraison.";
+    }
     
 
 ?>
